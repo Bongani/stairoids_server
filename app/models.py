28b658f660a00328892bbdd50393c2287ca48ec8
john.muller <john.muller@ing.com>
2018-09-10 13:12:13 +0200

    Add first working app
    Only hello world

--- app/models.py
+++ app/models.py
@@ -1,9 +1,17 @@
 from app import db
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+
+class UserBoard(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     fastest_time = db.Column(db.Integer, index=True, unique=True)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Events(db.Model):
+    event_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, index=True)
+    location = db.Column(db.Integer)
+    time = db.Column(db.Integer, index=True)
